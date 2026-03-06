830781d6c5b1568b0cedbbd65dbbc30b6474e18c
Naedson Valeria <lucassousagth@gmail.com>
2026-03-06 18:40:32 -0300

    Animações de ataque com combo e buffer de input

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -1,10 +1,12 @@
+
+
 using UnityEngine;
-
-
-
 public class PlayerAttack : MonoBehaviour{
-private int comboSteps = 0;
+private int comboSteps = 1;
 private Animator animator;
+private bool isAttack;
+private float bufferTime = 0.8f;
+private float bufferCount;
 
     void Start()
     {
@@ -14,16 +16,42 @@ private Animator animator;
     {
         if (Input.GetKeyDown(KeyCode.X))
         {
-            Debug.Log("Ataque");
+             
+            bufferCount = bufferTime;
+               
+        }
+        if(bufferCount > 0)
+        {
+            bufferCount -= Time.deltaTime;
+        }
+        if(bufferCount > 0 && !isAttack)
+        {
             
-            animator.SetInteger("attackClickCount",comboSteps);
+            isAttack = true;
+            bufferCount = 0;
+            RunAnimation();
+            
+            Invoke(nameof(ResetAttack), 0.6f);
+        }
+        
+    }
+    void RunAnimation()
+    {
+         Debug.Log(comboSteps);
+               animator.SetInteger("attackClickCount",comboSteps);
             animator.SetTrigger("attack");
             comboSteps++;
+         
+            
             if(comboSteps > 2)
             {
-                comboSteps = 0;
+                comboSteps =1;
             }
-        }
+            
+    }
+    void ResetAttack()
+    {
+        isAttack = false;
     }
 
 }
